49c954620a2e6341da51c2bbe3b3ce6fb196d143
Bohdan <bohdan.sell23@gmail.com>
2024-08-28 02:02:43 +0300

    ДЗ 4.2. Знайти суму елементів із парними індексами

--- Study.py
+++ Study.py
@@ -1,19 +1,30 @@
 
 
 
-import random
-lst = [random.randint(0,2) for _ in range(5)]
-print("Исходный списк:", lst)
-target_value = 0
-print("Значение для перемещения:", target_value)
-result = []
-zero_count = 0
-for x in lst:
-    if x == target_value:
-        zero_count += 1
-    else:
-        result.append(x)
-result.extend([target_value] * zero_count)
-print("Результат:", result)
+
+# target_value = 0
+# print("Значение для перемещения:", target_value)
+# result = []
+# zero_count = 0
+# for x in lst:
+#     if x == target_value:
+#         zero_count += 1
+#     else:
+#         result.append(x)
+# result.extend([target_value] * zero_count)
+# print("Результат:", result)
+# import random
+lst = [1,3,5]
+if not lst:
+    result = 0
+else:
+    sum_even_indices = 0
+    for i in range(0, len(lst), 2):
+        sum_even_indices += lst[i]
+    result = sum_even_indices * lst[-1]
+print(result)
+
+
+
 
 
