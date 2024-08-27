68cd2220f7f5cbf7cde5ad431893c783ee08e40b
Bohdan <bohdan.sell23@gmail.com>
2024-08-28 02:32:07 +0300

    ДЗ 4

--- Study.py
+++ Study.py
@@ -1,29 +1,36 @@
 
 
+4/1
+import random
 
 
-# target_value = 0
-# print("Значение для перемещения:", target_value)
-# result = []
-# zero_count = 0
-# for x in lst:
-#     if x == target_value:
-#         zero_count += 1
-#     else:
-#         result.append(x)
-# result.extend([target_value] * zero_count)
-# print("Результат:", result)
-# import random
-# lst = [1,3,5]
-# if not lst:
-#     result = 0
-# else:
-#     sum_even_indices = 0
-#     for i in range(0, len(lst), 2):
-#         sum_even_indices += lst[i]
-#     result = sum_even_indices * lst[-1]
-# print(result)
+lst = [random.randint(0, 2) for _ in range(5)]
+print("Исходный список:", lst)
+target_value = 0
+print("Значение для перемещения:", target_value)
+result = []
+zero_count = 0
+for x in lst:
+    if x == target_value:
+        zero_count += 1
+    else:
+        result.append(x)
+result.extend([target_value] * zero_count)
+print("Результат:", result)
 
+4/2
+import random
+lst = [1,3,5]
+if not lst:
+    result = 0
+else:
+    sum_even_indices = 0
+    for i in range(0, len(lst), 2):
+        sum_even_indices += lst[i]
+    result = sum_even_indices * lst[-1]
+print(result)
+
+4/3
 import random
 num_elements = random.randint(3, 10)
 random_list = [random.randint(0, 100) for _ in range(num_elements)]
