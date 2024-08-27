ec5c7c5d4916fea6dddc0663570c6aa17439410e
Bohdan <bohdan.sell23@gmail.com>
2024-08-28 01:14:03 +0300

    ДЗ 4.1. Перемістити всі нулі до кінця списку

---
+++ Study.py
@@ -0,0 +1,18 @@
+
+
+
+import random
+lst = [random.randint(0,2) for _ in range(5)]
+print("Исходный списк:", lst)
+target_value = 0
+print("Значение для перемещения:", target_value)
+result = []
+zero_count = 0
+for x in lst:
+    if x == target_value:
+        zero_count += 1
+    else:
+        result.append(x)
+result.extend([target_value] * zero_count)
+print("Результат:", result)
+
